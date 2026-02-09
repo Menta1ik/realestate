230b18731bfa304c125818e263ec7d3dca05be2f
Menta1ik <vitaly.fatsky@gmail.com>
2026-02-09 15:54:20 +0200

    fix: build errors, add missing deps and tsconfig settings

--- api/index.ts
+++ api/index.ts
@@ -1,7 +1,8 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from '../backend/src/app.module';
+import type { Request, Response } from 'express';
 
-export default async function handler(req, res) {
+export default async function handler(req: Request, res: Response) {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api');
   app.enableCors();
